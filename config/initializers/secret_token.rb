1d5b49fab2c6b7af16f42e4f49b0b52086b63c6c
roxanne <roxanne.pirate@gmail.com>
2013-07-02 10:37:31 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Junemovie::Application.config.secret_token = 'f3e751a0b23b4b0c9a1384b50418c73d895e288d82f17b635d8d5ba89680f76bfe670ebda774c3e81d7c154c58a7f813dee666fc60ee4875a02576f9f1e9efc3'
