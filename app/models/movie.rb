9b4dee93b725825adf06be9e73a9519cc91f1566
yuanwallace <yuanwallace@gmail.com>
2013-07-09 15:40:48 +0800

    在movie后台添加文件上传方法

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,7 +12,9 @@
 #
 
 class Movie < ActiveRecord::Base
-  attr_accessible :name, :poster, :summary, :video_file
+  attr_accessible :name, :poster, :summary, :video_file, :avatar
+
+  mount_uploader :avatar, AvatarUploader
 
   has_many :comments
 
