043dffa6f5fdbe3cf53d71dea1029dfce06d1a3e
lily <1992zhang2018@gmail.com>
2013-07-10 20:14:11 +0800

    前台页面链接

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Junemovie::Application.routes.draw do
-
-
-
   devise_for :users
 
   root :to => "welcome#index"
   get 'welcome/index' => "welcome#index"
 
-  resources :news
+  resources :titles
   
   resources :movies do
     resources :posts, :comments
